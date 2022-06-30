37340501bbaae14391b16760fc0d6f66d522f145
lidotcircle <beam.analyman@gmail.com>
2022-06-30 12:34:21 +0800

    save

--- train.py
+++ train.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
             iter_data_time = time.time()
 
         if epoch >= opt.metric_start_epoch and epoch % opt.metric_eval_freq == 0:
-            metrics_stats_array = model.eval_metrics_no()
+            metrics_stats_array = model.eval_metrics(epoch=epoch, num_test=1000)
             send_stats = {}
             send_stats['epoch'] = epoch
             for i in range(len(metrics_stats_array)):
