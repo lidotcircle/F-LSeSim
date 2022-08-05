01ffaacf919c30d2223a165498c8251be295782f
lidotcircle <beam.analyman@gmail.com>
2022-08-05 18:25:41 +0800

    new generator

--- models/networks.py
+++ models/networks.py
@@ -9,7 +9,7 @@ from .transtyle import Transtyle, TransDiscriminator
 from .pregan_networks import MultiYAPatch, ResnetGenerator as ResnetPre, ResnetGeneratorV2 as ResnetPreAE, ResnetGeneratorV3 as ResnetAE, ResnetSepGenerator
 from .nlayer_prefocus_discriminator import NLayerPreFocusDiscriminator
 from .cvt import CvTGenerator, CvTDiscriminator
-from .scg_networks import Generator as FGGenerator
+from .scg_networks import Generator as FGGenerator, FGGenerator as FFGGenerator
 
 
 ##################################################################################
@@ -48,6 +48,8 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = ResnetAE(input_nc, output_nc, ngf, n_blocks=4)
     elif netG == 'fastgan':
         net = FGGenerator(ngf=ngf)
+    elif netG == 'fgen':
+        net = FFGGenerator(ngf=ngf)
     elif netG == 'resnet_preae':
         net = ResnetPreAE(
             input_nc, output_nc, ngf, n_blocks=opt.g_num_layers,
--- models/scg_networks.py
+++ models/scg_networks.py
@@ -1,6 +1,7 @@
 import torch.nn.functional as F 
 from torch import nn
 from pg_modules import blocks
+from pg_modules.blocks import UpBlockBig, UpBlockSmall, DownBlock, SEBlock, conv2d
 from pg_modules.networks_fastgan import FastganSynthesis
 
 
@@ -47,3 +48,83 @@ class Generator(nn.Module):
             latent_out.append(latent)
 
         return self.decoder(latent.unsqueeze(1), c=None)
+
+
+class FGGenerator(nn.Module):
+    def __init__(self, ngf=128, img_resolution=256, nc=3, lite=False):
+        super().__init__()
+        self.img_resolution = img_resolution
+        nfc_multi = {2: 16, 4:16, 8:8, 16:4, 32:2, 64:2, 128:1, 256:0.5,
+                     512:0.25, 1024:0.125}
+        nfc = {}
+        for k, v in nfc_multi.items():
+            nfc[k] = int(v*ngf)
+
+        assert img_resolution in nfc
+        self.init_layer = nn.Conv2d(3, nfc[img_resolution], kernel_size=7, stride=1, padding=3)
+        if img_resolution > 512:
+            self.dfeat_1024 = self.init_layer
+            self.dfeat_512 = DownBlock(nfc[1024], nfc[512])
+        elif img_resolution == 512:
+            self.dfeat_512 = self.init_layer
+        self.dfeat_256 = self.init_layer if img_resolution == 256 else DownBlock(nfc[512], nfc[256])
+        self.dfeat_128 = DownBlock(nfc[256], nfc[128])
+        self.dfeat_64 = DownBlock(nfc[128], nfc[64])
+        self.dfeat_32 = DownBlock(nfc[64], nfc[32])
+        self.dfeat_16 = DownBlock(nfc[32], nfc[16])
+        self.dfeat_8 = DownBlock(nfc[16], nfc[8])
+        self.dfeat_4 = DownBlock(nfc[8], nfc[4])
+
+        UpBlock = UpBlockSmall if lite else UpBlockBig
+        self.feat_8   = UpBlock(nfc[4], nfc[8])
+        self.feat_16  = UpBlock(nfc[8], nfc[16])
+        self.feat_32  = UpBlock(nfc[16], nfc[32])
+        self.feat_64  = UpBlock(nfc[32], nfc[64])
+        self.feat_128 = UpBlock(nfc[64], nfc[128])
+        self.feat_256 = UpBlock(nfc[128], nfc[256])
+
+        self.se_64  = SEBlock(nfc[64], nfc[64])
+        self.se_128 = SEBlock(nfc[32], nfc[128])
+        self.se_256 = SEBlock(nfc[16], nfc[256])
+
+        self.to_big = conv2d(nfc[img_resolution], nc, 3, 1, 1, bias=True)
+
+        if img_resolution > 256:
+            self.feat_512 = UpBlock(nfc[256], nfc[512])
+            self.se_512 = SEBlock(nfc[8], nfc[512])
+        if img_resolution > 512:
+            self.feat_1024 = UpBlock(nfc[512], nfc[1024])
+
+    def forward(self, img, **kwargs):
+        feat = img
+        if self.img_resolution >= 1024:
+            feat = self.dfeat_1024(feat)
+        if self.img_resolution >= 512:
+            feat = self.dfeat_512(feat)
+        dfeat_256 = self.dfeat_256(feat)
+        dfeat_128 = self.dfeat_128(dfeat_256)
+        dfeat_64 = self.dfeat_64(dfeat_128)
+        dfeat_32 = self.dfeat_32(dfeat_64)
+        dfeat_16 = self.dfeat_16(dfeat_32)
+        dfeat_8 = self.dfeat_8(dfeat_16)
+        dfeat_4 = self.dfeat_4(dfeat_8)
+
+        feat_8 = self.feat_8(dfeat_4)
+        feat_16 = self.feat_16(feat_8)
+        feat_32 = self.feat_32(feat_16)
+        feat_64 = self.se_64(dfeat_64, self.feat_64(feat_32))
+        feat_128 = self.se_128(dfeat_32,  self.feat_128(feat_64))
+
+        if self.img_resolution >= 128:
+            feat_last = feat_128
+
+        if self.img_resolution >= 256:
+            feat_last = self.se_256(dfeat_16, self.feat_256(feat_last))
+
+        if self.img_resolution >= 512:
+            feat_last = self.se_512(feat_8, self.feat_512(feat_last))
+
+        if self.img_resolution >= 1024:
+            feat_last = self.feat_1024(feat_last)
+
+        return self.to_big(feat_last)
